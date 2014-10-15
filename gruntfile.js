2f112dc183cc224087247bce798d2bff8e2b5e9f
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2014-10-15 15:15:54 +0200

    Start using Grunt

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Tasks.
+  grunt.registerTask('default', ['jshint']);
+
+};
